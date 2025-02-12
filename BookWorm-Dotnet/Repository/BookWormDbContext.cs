1a828d10632b24a6d75d1b3d4e40a392aa6c5356
PrashantSGour <154967323+PrashantSGour@users.noreply.github.com>
2025-02-12 13:56:31 +0530

    Folders created

--- BookWorm-Dotnet/Repository/BookWormDbContext.cs
+++ BookWorm-Dotnet/Repository/BookWormDbContext.cs
@@ -1,20 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using BookWorm_Dotnet.Models;
 using Microsoft.EntityFrameworkCore;
 using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
 
-namespace BookWorm_Dotnet.Models;
+namespace BookWorm_Dotnet.Repository;
 
 public partial class BookWormDbContext : DbContext
 {
-    public BookWormDbContext()
-    {
-    }
 
     public BookWormDbContext(DbContextOptions<BookWormDbContext> options)
         : base(options)
     {
     }
+
+    public DbSet<LogEntry> LogEntries { get; set; }  // Log Table
 
     public virtual DbSet<AttributeMaster> AttributeMasters { get; set; }
 
@@ -52,13 +52,14 @@ public partial class BookWormDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;database=bookworm;user=root;password=root", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.36-mysql"));
+        => optionsBuilder.UseMySql("server=localhost;database=bookworm;user=root;password=root", ServerVersion.Parse("8.0.36-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder
             .UseCollation("utf8mb4_0900_ai_ci")
             .HasCharSet("utf8mb4");
+
 
         modelBuilder.Entity<AttributeMaster>(entity =>
         {
@@ -208,6 +209,9 @@ public partial class BookWormDbContext : DbContext
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FKas49u6dxu8mu28ylsq0cucx1b");
         });
+
+        modelBuilder.Entity<LogEntry>().ToTable("log_entries");
+
 
         OnModelCreatingPartial(modelBuilder);
     }
---
+++ BookWorm-Dotnet/Services/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BookWorm_Dotnet.Models;
+
+namespace BookWorm_Dotnet.Services
+{
+    public interface IProductService
+    {
+        Task<List<ProductMaster>> GetAllProductsAsync();
+        Task<ProductMaster?> GetProductByIdAsync(int productId);
+        Task<ProductMaster> AddProductAsync(ProductMaster product);
+        Task<ProductMaster?> UpdateProductAsync(ProductMaster product);
+        Task<bool> DeleteProductAsync(int productId);
+    }
+}
